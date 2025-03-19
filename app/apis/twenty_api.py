880ecbe79a41e021d62f1c505a329be041c228ae
Renato Mendes <mendeslrenato@gmail.com>
2025-03-19 19:11:42 -0300

    feat: add TwentyApi client

---
+++ app/apis/twenty_api.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+
+class TwentyApi:
+    def __init__(self):
+        self.base_url = "http://localhost:3000/rest"
+        self.token = os.getenv("TWENTY_API_KEY")
+
+    def get_opportunities(self):
+        end_point = f"{self.base_url}/opportunities"
+        headers = {"Authorization": f"Bearer {self.token}"}
+        response = requests.get(end_point, headers=headers)
+        if response.status_code != 200:
+            return []
+        data = response.json()
+        return data["data"]["opportunities"]
