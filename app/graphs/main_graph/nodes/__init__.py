e930e6dcf24ef1e687eb441f559644092c51a9d5
Renato Mendes <mendeslrenato@gmail.com>
2025-03-19 19:16:39 -0300

    feat: add graph assistant and route_tools node

---
+++ app/graphs/main_graph/nodes/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa F401
+
+from .assistant import assistant_node
+from .route_tools import route_tools_node
---
+++ app/graphs/main_graph/nodes/assistant.py
@@ -0,0 +1,17 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage
+
+from graphs.main_graph.states import State
+from graphs.main_graph.prompts import assistant_prompt
+from graphs.main_graph.tools import tools
+
+
+def assistant_node(state: State):
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.0)
+    llm_with_tools = llm.bind_tools(tools)
+    messages = state["messages"]
+    return {
+        "messages": [
+            llm_with_tools.invoke([SystemMessage(content=assistant_prompt)] + messages)
+        ]
+    }
