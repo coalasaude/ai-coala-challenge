4e108aba36963671650069fb51b57d111c0bfce0
Renato Mendes <mendeslrenato@gmail.com>
2025-03-20 16:48:12 -0300

    feat: add persistance to the IA chat

--- app/graphs/main_graph/__init__.py
+++ app/graphs/main_graph/__init__.py
@@ -1,9 +1,16 @@
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode
+from langgraph.checkpoint.postgres import PostgresSaver
+from dotenv import load_dotenv
+from psycopg import Connection
+import os
 
 from graphs.main_graph.states import State
 from graphs.main_graph.nodes import assistant_node, route_tools_node
 from graphs.main_graph.tools import tools
+from graphs.main_graph.utils.is_checkpoint_first_time import is_first_time_setup
+
+load_dotenv(override=True)
 
 main_graph_builder = StateGraph(State)
 
@@ -16,4 +23,11 @@ main_graph_builder.add_conditional_edges(
     "assistant", route_tools_node, {"tools": "tools", END: END}
 )
 
-main_graph = main_graph_builder.compile()
+db_uri = os.getenv("PG_DATABASE_URL")
+conn = Connection.connect(db_uri)
+checkpointer = PostgresSaver(conn)
+
+if is_first_time_setup(db_uri):
+    checkpointer.setup()
+
+main_graph = main_graph_builder.compile(checkpointer=checkpointer)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import Optional
+from langchain_core.runnables.config import RunnableConfig
 from dotenv import load_dotenv
+from uuid import uuid4
 
 from graphs.main_graph import main_graph
+
 
 load_dotenv(override=True)
 
@@ -11,23 +15,29 @@ app = FastAPI()
 
 class GraphInput(BaseModel):
     query: str
-    parameters: dict = {"thread_id": 1}
+    parameters: Optional[RunnableConfig] = None
 
 
 @app.post("/invoke")
 async def invoke(graph_input: GraphInput):
     try:
+        parameters = graph_input.parameters
+        if parameters is None:
+            thread_id = str(uuid4())
+            parameters = {"configurable": {"thread_id": thread_id}}
+
         result = main_graph.invoke(
             {"messages": [graph_input.query]},
-            graph_input.parameters,
+            parameters,
             stream_mode="values",
         )
         return {
-            "result": [
-                message.content
+            "thread_id": parameters["configurable"]["thread_id"],
+            "messages": [
+                {"type": message.type, "content": message.content}
                 for message in result["messages"]
-                if message.type in ("ai", "human")
-            ]
+                if message.type in ("ai", "human") and message.content
+            ],
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Graph execution failed: {str(e)}")
